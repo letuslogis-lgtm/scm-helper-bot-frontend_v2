af347067c3aaf4f672b67a1b37ad76e3592aa4d3
Letus Logis User <admin@letuslogis.com>
2026-05-14 17:49:34 +0900

    근무자 관리: 일괄 수정 모달 근무지 목록 누락분 업데이트

--- src/WorkerModals.jsx
+++ src/WorkerModals.jsx
@@ -602,7 +602,7 @@ const WorkerBulkEditModal = ({ selectedIds, workers, vendorList, onClose, onRelo
                                         <option value="">선택 안함</option>
                                         <option value="양지1센터">양지1센터</option><option value="양지2센터">양지2센터</option><option value="양지3센터">양지3센터</option>
                                         <option value="안성센터">안성센터</option><option value="평택센터">평택센터</option><option value="음성센터">음성센터</option>
-                                        <option value="동부센터">동부센터</option><option value="서부센터">서부센터</option>
+                                        <option value="대전센터">대전센터</option><option value="대구센터">대구센터</option><option value="부산센터">부산센터</option><option value="광주센터">광주센터</option>
                                     </select>
                                     <select value={empType} onChange={(e) => setEmpType(e.target.value)} className="border border-gray-300 rounded px-2.5 py-1.5 text-[11px] outline-none flex-1 bg-white cursor-pointer">
                                         <option value="현장직">현장직</option>
